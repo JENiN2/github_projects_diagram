a243ae46dc6c3aee638c0157b51a171c5841be96
Ruslan Nazarov <hlfan2@yandex.ru>
2022-01-26 19:46:03 +0700

    render repository diagram

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Создание вызова API и сохранение ответа.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -13,11 +15,15 @@ print('Total Repositories', response_dict['total_count'])
 
 # Анализ информации о репозиториях.
 repo_dicts = response_dict['items']
-print('Repositories Returned:', len(repo_dicts))
-print('\nSelected information about each repository:')
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('\nName:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Построение визуализации.
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
